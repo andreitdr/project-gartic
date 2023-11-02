12d411d6b4eea5a5d6f610f946c8ad38063b7924
Verdeș George-Alexandru <george.verdes@student.unitbv.ro>
2023-11-02 22:23:04 +0200

    Added the login method.

--- Backend/UserLogin.cpp
+++ Backend/UserLogin.cpp
@@ -4,3 +4,18 @@ UserLogin::UserLogin(const User& user, const Credentials& credentials) :
 	m_user(user), m_credentials(credentials) 
 {}
 
+bool UserLogin::login(const std::string& givenPassword)
+{
+	if (m_user.getUsername() != m_credentials.getUsername())
+	{
+		return false;
+	}
+
+	if (m_credentials.verifyPassword(givenPassword))
+	{
+		return true;
+	}
+	
+	return false;
+}
+
--- Backend/UserLogin.h
+++ Backend/UserLogin.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "User.h"
 #include "Credentials.h"
+#include <string>
+
 class UserLogin
 {
 public:
 	UserLogin(const User& user, const Credentials& credentials);
+	bool login(const std::string& givenPassword);
 
 private:
 	User m_user;
