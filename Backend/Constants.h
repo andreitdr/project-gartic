42ea8465a8cc956f47cb0b323047db948bf50c07
Andrei Tudor <tudor.andrei.707@gmail.com>
2023-12-09 16:34:12 +0000

    Fixed responses

--- Backend/Constants.h
+++ Backend/Constants.h
@@ -1,11 +1,5 @@
 #pragma once
 #include <iostream>
 import Logger;
-/**
- * \brief Error message for incorrect formatted JSON string
- */
-#define K_CROW_ERROR_INVALID_JSON crow::response(200, "The json was in an incorrect format")
-#define K_CROW_ERROR_USER_NOT_FOUND crow::response(200, "The user was not found")
-
 
 static Logger k_logger(std::cout,Logger::Level::Debug,"log.txt");
--- Backend/main.cpp
+++ Backend/main.cpp
@@ -1,4 +1,6 @@
 ﻿import Config;
+import BaseResponse;
+
 #include <crow.h>
 #include "SqlDatabase/SqlDatabase.h"
 
@@ -17,10 +19,10 @@ using namespace sqlite_orm;
 void tests()
 {
     ConfigFile config_file("./test.ini");
-    config_file.WriteConfig("key1","value");
-    config_file.WriteConfig("key1","value2");
-    config_file.WriteConfig("key2","value3");
-    std::cout<<config_file.ReadConfig("key3");
+    config_file.WriteConfig("key1", "value");
+    config_file.WriteConfig("key1", "value2");
+    config_file.WriteConfig("key2", "value3");
+    std::cout << config_file.ReadConfig("key3");
 }
 
 int main()
@@ -28,89 +30,122 @@ int main()
 #if ENABLETEST
     tests();
 #else
-    
+
     SqlDatabase::Init();
     crow::SimpleApp app;
 
     k_logger.LogMessage("Starting server...", "MAIN");
 
-    CROW_ROUTE(app, "/user/get_user")
-    .methods("GET"_method)
-    ([](const crow::request& request)
+    CROW_ROUTE(app, "/user/get_user").methods("GET"_method)([](const crow::request& request)
     {
         const auto json = crow::json::load(request.body);
-        if(!json) return K_CROW_ERROR_INVALID_JSON;
+        if (!json)
+        {
+            BaseResponse response;
+            response.m_successState = false;
+            response.AppendMessage("The json was in an incorrect format");
+
+            auto responseJson = JsonConvertor::ConvertBaseResponse(response);
+            return crow::response(responseJson);
+        }
 
         auto response = GetUserInfo(json);
         return crow::response(response);
     });
 
-    CROW_ROUTE(app, "/game/create_game")
-        .methods("POST"_method)
-    ([](const crow::request& request)
+    CROW_ROUTE(app, "/game/create_game").methods("POST"_method)([](const crow::request& request)
     {
         const auto json = crow::json::load(request.body);
 
         if (!json)
-            return K_CROW_ERROR_INVALID_JSON;
+        {
+            BaseResponse response;
+            response.m_successState = false;
+            response.AppendMessage("The json was in an incorrect format");
+
+            auto responseJson = JsonConvertor::ConvertBaseResponse(response);
+            return crow::response(responseJson);
+        }
 
         auto response = StartGame(json);
-        return crow::response(response);   
+        return crow::response(response);
     });
 
-    CROW_ROUTE(app, "/game/create_lobby")
-        .methods("POST"_method)
-    ([](const crow::request& request)
+    CROW_ROUTE(app, "/game/create_lobby").methods("POST"_method)([](const crow::request& request)
     {
         const auto json = crow::json::load(request.body);
 
         if (!json)
-            return K_CROW_ERROR_INVALID_JSON;
+        {
+            BaseResponse response;
+            response.m_successState = false;
+            response.AppendMessage("The json was in an incorrect format");
+
+            auto responseJson = JsonConvertor::ConvertBaseResponse(response);
+            return crow::response(responseJson);
+        }
 
         auto response = CreateLobby(json);
-        return crow::response(response);   
-    });
-
-    CROW_ROUTE(app, "/user/register")
-    .methods("POST"_method)
-    ([](const crow::request& request){
-        const auto json = crow::json::load(request.body);
-        
-        if(!json)
-            return K_CROW_ERROR_INVALID_JSON;
-        
-        auto response =  (RegisterUser(json));
         return crow::response(response);
     });
-    
-    CROW_ROUTE(app, "/user/login")
-    .methods("GET"_method)
-    ([](const crow::request& request)
-    {
-       const auto json = crow::json::load(request.body);
 
-        if(!json)
-            return K_CROW_ERROR_INVALID_JSON;
+    CROW_ROUTE(app, "/user/register").methods("POST"_method)([](const crow::request& request)
+    {
+        const auto json = crow::json::load(request.body);
+
+        if (!json)
+        {
+            BaseResponse response;
+            response.m_successState = false;
+            response.AppendMessage("The json was in an incorrect format");
+
+            auto responseJson = JsonConvertor::ConvertBaseResponse(response);
+            return crow::response(responseJson);
+        }
+
+        auto response = (RegisterUser(json));
+        return crow::response(response);
+    });
+
+    CROW_ROUTE(app, "/user/login").methods("GET"_method)([](const crow::request& request)
+    {
+        const auto json = crow::json::load(request.body);
+
+        if (!json)
+        {
+            BaseResponse response;
+            response.m_successState = false;
+            response.AppendMessage("The json was in an incorrect format");
+
+            auto responseJson = JsonConvertor::ConvertBaseResponse(response);
+            return crow::response(responseJson);
+        }
 
         std::string username = json["username"].s();
 
         auto users = SqlDatabase::GetAll<User>(where(c(&User::m_username) == username));
-        if(users.empty())
-            return K_CROW_ERROR_USER_NOT_FOUND;
+        if (users.empty())
+        {
+            BaseResponse response;
+            response.m_successState = false;
+            response.AppendMessage("Inexistent user");
 
-        
-        auto wjson =UserLogin(json);
+            auto responseJson = JsonConvertor::ConvertBaseResponse(response);
+            return crow::response(responseJson);
+        }
+
+
+        auto wjson = UserLogin(json);
 
         wjson["UserData"] = JsonConvertor::ConvertUser(users[0], true);
-        
+
         return crow::response(wjson);
     });
-    
+
     app.loglevel(crow::LogLevel::Error);
     k_logger.LogMessage("Server is running at http://localhost:18080", "MAIN");
     app.port(18080).multithreaded().run();
 #endif
-    
+
     return 0;
-    
 }
