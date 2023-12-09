a40dd945e0339675a044910fc45c91fff75eca06
Andrei Tudor <tudor.andrei.707@gmail.com>
2023-12-09 16:18:19 +0000

    Fixed response codes

--- Backend/Constants.h
+++ Backend/Constants.h
@@ -2,10 +2,10 @@
 #include <iostream>
 import Logger;
 /**
- * \brief Error message for incorrect formated JSON string
+ * \brief Error message for incorrect formatted JSON string
  */
-#define K_CROW_ERROR_INVALID_JSON crow::response(400, "The json was in an incorrect format")
-#define K_CROW_ERROR_USER_NOT_FOUND crow::response(400, "The user was not found")
+#define K_CROW_ERROR_INVALID_JSON crow::response(200, "The json was in an incorrect format")
+#define K_CROW_ERROR_USER_NOT_FOUND crow::response(200, "The user was not found")
 
 
 static Logger k_logger(std::cout,Logger::Level::Debug,"log.txt");
