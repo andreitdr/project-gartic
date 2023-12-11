e931e0fbacaeed84b4d193e4f0d393c0d8e0e0b5
Andrei Pintea <andrei.pintea@student.unitbv.ro>
2023-12-11 14:53:52 +0200

    updated main to also have leave lobby route

--- Backend/main.cpp
+++ Backend/main.cpp
@@ -7,6 +7,7 @@ import BaseResponse;
 #include "API/Game/CreateLobby.h"
 #include "API/Game/StartGame.h"
 #include "API/Game/JoinLobby.h"
+#include "API/Game/LeaveLobby.h"
 
 #include "API/User/GetUserInfo.h"
 #include "API/User/UserLogin.h"
@@ -51,6 +52,23 @@ int main()
         }
 
         auto response = JoinLobby(json);
+        return crow::response(response);
+    });
+
+    CROW_ROUTE(app, "/game/leave_lobby").methods("POST"_method)([](const crow::request& request)
+    {
+       const auto json = crow::json::load(request.body);
+        if (!json)
+        {
+            BaseResponse response;
+            response.m_successState = false;
+            response.AppendMessage("The json was in an incorrect format");
+
+            auto responseJson = JsonConvertor::ConvertBaseResponse(response);
+            return crow::response(responseJson);
+        }
+
+        auto response = LeaveLobby(json);
         return crow::response(response);
     });
 
