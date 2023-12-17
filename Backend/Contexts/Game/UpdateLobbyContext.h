96a2ff0558773d772fa9d3c53b44bf1f3fe409c1
Verdeș George-Alexandru <george.verdes@student.unitbv.ro>
2023-12-17 20:12:50 +0200

    added UpdateLobbyContext::ApplyChanges

--- Backend/Contexts/Game/UpdateLobbyContext.h
+++ Backend/Contexts/Game/UpdateLobbyContext.h
@@ -49,3 +49,24 @@ inline UpdateLobbyResponse UpdateLobbyContext::ValidateData(const UpdateLobbyReq
         return UpdateLobbyResponse(error.what());
     }
 }
+
+inline UpdateLobbyResponse UpdateLobbyContext::ApplyChanges(const UpdateLobbyRequest& request)
+{
+    int lobbyId = request.GetLobbyId();
+    int newLeaderId = request.GetNewLeaderId();
+
+    try
+    {
+        Lobby currentLobby = SqlDatabase::Get<Lobby>(WHERE(Lobby::m_lobbyId, lobbyId));
+
+        currentLobby.m_leaderId = newLeaderId;
+
+        SqlDatabase::Update(currentLobby);
+
+        return UpdateLobbyResponse();
+    }
+    catch (const std::system_error& error)
+    {
+        return UpdateLobbyResponse(error.what());
+    }
+}
