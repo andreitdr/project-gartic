ba31e8cc3b4262551fef31f06f5e6fb51cf33811
Verdeș George-Alexandru <george.verdes@student.unitbv.ro>
2023-12-17 20:02:33 +0200

    added UpdateLobbyRequest implementation

--- Backend/Infrastructure/Lobby/UpdateLobbyRequest.h
+++ Backend/Infrastructure/Lobby/UpdateLobbyRequest.h
@@ -12,3 +12,18 @@ private:
     int m_lobbyId;
     int m_newLeaderId;
 };
+
+inline UpdateLobbyRequest::UpdateLobbyRequest(const int lobbyId, const int newLeaderId)
+    : m_lobbyId(lobbyId), m_newLeaderId(newLeaderId)
+{
+}
+
+inline int UpdateLobbyRequest::GetLobbyId() const
+{
+    return m_lobbyId;
+}
+
+inline int UpdateLobbyRequest::GetNewLeaderId() const
+{
+    return m_newLeaderId;
+}
