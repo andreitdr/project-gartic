221e640eb65be48223f6e4d9bebdb7096ce712fd
Verdeș George-Alexandru <george.verdes@student.unitbv.ro>
2023-12-17 20:05:48 +0200

    added UpdateLobbyResponse implementation

--- Backend/Infrastructure/Lobby/UpdateLobbyResponse.h
+++ Backend/Infrastructure/Lobby/UpdateLobbyResponse.h
@@ -16,3 +16,20 @@ private:
     bool m_successState;
     std::string m_message;
 };
+
+inline UpdateLobbyResponse::UpdateLobbyResponse() : m_successState(true) {}
+
+inline UpdateLobbyResponse::UpdateLobbyResponse(const std::string& message)
+    : m_successState(false), m_message(message)
+{
+}
+
+inline bool UpdateLobbyResponse::GetSuccessState() const
+{
+    return m_successState;
+}
+
+inline const std::string& UpdateLobbyResponse::GetMessage() const
+{
+    return m_message;
+}
