3d026a7c4d069118b44b11b05602392bf4c4293c
Verdeș George-Alexandru <george.verdes@student.unitbv.ro>
2024-01-01 14:10:11 +0200

    added JoinRandomLobbyContext::ApplyChanges

--- Backend/Contexts/Game/JoinRandomLobbyContext.h
+++ Backend/Contexts/Game/JoinRandomLobbyContext.h
@@ -20,3 +20,40 @@ inline JoinRandomLobbyResponse JoinRandomLobbyContext::HandleRequest(const JoinR
 {
     return ApplyChanges(request);
 }
+
+inline JoinRandomLobbyResponse JoinRandomLobbyContext::ApplyChanges(const JoinRandomLobbyRequest& request)
+{
+    try
+    {
+        int userId = request.GetUserId();
+        
+        auto allLobbies = SqlDatabase::GetAll<Lobby>(sqlite_orm::where(sqlite_orm::c(&Lobby::m_index) > 0));
+
+        if (!allLobbies.empty())
+        {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<size_t> dis(0, allLobbies.size() - 1);
+
+            size_t randomIndex = dis(gen);
+
+            Lobby randomLobby = allLobbies[randomIndex];
+
+            std::vector<int> userIds = JsonConvertor::ConvertToVector<int>(randomLobby.m_userIds);
+            userIds.emplace_back(userId);
+            randomLobby.m_userIds = JsonConvertor::ConvertFromVector(userIds).dump();
+
+            SqlDatabase::Update(randomLobby);
+
+            return JoinRandomLobbyResponse();
+        }
+        else
+        {
+            return JoinRandomLobbyResponse("No available lobbies found.");
+        }
+    }
+    catch (const std::system_error& error)
+    {
+        return JoinRandomLobbyResponse(error.what());
+    }
+}
