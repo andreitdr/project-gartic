92a5aff89fc66b5b00a56ebbcd337db2ad6fd3b9
alexandru-pestritu <147277107+alexandru-pestritu@users.noreply.github.com>
2024-01-12 14:55:56 +0200

    Implemented drawing converter

---
+++ Frontend/Utils/QImageConverter/QImageConverter.cpp
@@ -0,0 +1,31 @@
+#include "QImageConverter.h"
+
+QImageConverter::QImageConverter()
+{
+}
+
+QImageConverter::~QImageConverter()
+{
+}
+
+std::string QImageConverter::convertImageToString(const QImage& image)
+{
+    QByteArray byteArray;
+    QBuffer buffer(&byteArray);
+    buffer.open(QIODevice::WriteOnly);
+    image.save(&buffer, "PNG");
+
+    std::string encodedData = byteArray.toBase64().toStdString();
+
+    return encodedData;
+}
+
+QImage QImageConverter::convertStringToImage(const std::string& encodedString)
+{
+    QByteArray byteArray = QByteArray::fromBase64(encodedString.c_str());
+
+    QImage image;
+    image.loadFromData(byteArray, "PNG");
+
+    return image;
+}
