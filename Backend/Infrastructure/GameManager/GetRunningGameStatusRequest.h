99491cd2067e4af68dd6c6744824d8431d2c5267
Andrei Tudor <tudor.andrei.707@gmail.com>
2024-01-13 19:02:31 +0200

    Updated dirs

--- Backend/Infrastructure/GameManager/GetRunningGameStatusRequest.h
+++ Backend/Infrastructure/GameManager/GetRunningGameStatusRequest.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-class RunningGameStatusRequest
+class GetRunningGameStatusRequest
 {
 public:
     
--- Backend/Infrastructure/GameManager/GetRunningGameStatusResponse.h
+++ Backend/Infrastructure/GameManager/GetRunningGameStatusResponse.h
@@ -1,6 +1,6 @@
 ﻿#pragma once
 
-class RunningGameStatusResponse
+class GetRunningGameStatusResponse
 {
 public:
     
