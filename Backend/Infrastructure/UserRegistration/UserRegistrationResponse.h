413c4864aaa12e6ab5ffefb60386974448e3fa91
Andrei Tudor <tudor.andrei.707@gmail.com>
2024-01-14 11:13:44 +0200

    Removed optionals

--- Backend/Infrastructure/UserRegistration/UserRegistrationResponse.h
+++ Backend/Infrastructure/UserRegistration/UserRegistrationResponse.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <string>
-#include <optional>
 
 #include "../../Infrastructure/BaseResponse.h"
 #include "../../DataTypes/User/User.h"
@@ -13,8 +12,8 @@ public:
     UserRegistrationResponse(const User& user);
 
     void SetUser(const User& user);
-    std::optional<User> GetUser() const;
+    User GetUser() const;
 
 private:
-    std::optional<User> m_user;
+    User m_user;
 };
